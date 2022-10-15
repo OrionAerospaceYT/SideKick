049b005ba56319696b6a6103d5d8a66d5e539ca9
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-10-15 09:21:45 +0100

    DeviceManager
    New device manager with a threaded get data loop.

--- DeviceManager.py
+++ DeviceManager.py
@@ -7,71 +7,91 @@ import threading
 # COM ports for the main backend
 class DeviceManager():
 
-    # initialisation of variables and threads the
-    # get raw data
+    # initialisation of variables
     def __init__(self):
         self.device = None
         self.rawData = ""
 
-        self.terminalData = []
+        self.terminalData = ""
         self.graphTopData = []
         self.graphBottomData = []
 
-        getData = threading.Thread(target=self.threadedGetRawData, args=(),)
-        getData.start()
-
     # this function runs on a thread and only gets the
-    # raw input dat from the com port as a binary string
+    # raw input data from the com port as a binary string
     # if the device is connected
-    def threadedGetRawData(self):
-        while True:
-            if self.device != None:
-                self.rawData = self.device.readline()
-                self.rawData = str(self.rawData)
-                
-    # connects the device in order to read the data coming
-    # from the SideKick/Teensy/Arduino
-    def connectDevice(self, port="COM1", baud=115200):
+    def threadedGetRawData(self, port, baud):
         self.device = serial.Serial(port, baud, timeout=0, rtscts=False)
+        while self.device != None:
+            rawData = self.device.readline().decode("utf-8")
+            if "\n" in rawData:
+                self.rawData = rawData
 
     # parses the raw data in the form of "g(name,1,data)t(text)g(name,2,data)"
-    # to terminalData, graphTopData, graphBottomData which are string, list,
-    # list respectively.
+    # to graphTopData, graphBottomData which are lists
     # example graph output would be: [[1,2,3,4,5,],[5,4,3,2,1],[2,5,4,1,3]]
-    def decodeData(self):
-
+    #
+    # this data still needs to be processed as it is in string form
+    # the processing will go in the main backend
+    def decodeGraphData(self):
         rawList = self.rawData.split("g(")
-        
+
         for data in rawList:
             data = data.split(")")
             if "t(" not in data[0] and data[0] != "" and "\r" not in data[0]:
                 validGraphData = data[0].replace(" ", "").split(",")
-                if validGraphData[1] == "1":
-                    self.graphTopData.append(validGraphData[2])
-                if  validGraphData[1] == "2":
-                    self.graphBottomData.append(validGraphData[2])
-                
+                self.graphTopData.append(validGraphData[2]) if validGraphData[1] == "1" else self.graphBottomData.append(validGraphData[2])
+
+        return self.graphTopData, self.graphBottomData
+
+    # parses the raw data in the form of "g(name,1,data)t(text)g(name,2,data)"
+    # to terminalData which is a string
+    # all terminal data from one output is put into a single line
+    def decodeTerminalData(self):
         rawList = self.rawData.split("t(")
         self.terminalData = ""
-        
+
         for data in rawList:
             data = data.split(")")
             if "g(" not in data[0] and data[0] != "" and "\r" not in data[0]:
-                self.terminalData += data[0] + " "
-            
-        return self.graphTopData, self.graphBottomData, self.terminalData
+                self.terminalData += " " + data[0]
 
-    
+        return self.terminalData
+
+    # terminates the while True loop to disconnect the device
+    # then sets the variables back to the initial states so
+    # that there is no data
+    def terminateDevice(self):
+        self.__init__()
+
+    # connects the device in order to read the data coming
+    # from the SideKick/Teensy/Arduino
+    # also stars the thread to get the data from the newly connected
+    # device
+    def connectDevice(self, port="COM1", baud=115200):
+        self.getData = threading.Thread(target=self.threadedGetRawData, args=(port, baud),)
+        self.getData.start()
+
 # only runs if this is the file being run
 # the unit test will go here
 if __name__ == "__main__":
 
+
     deviceManager = DeviceManager()
-    
-    deviceManager.rawData = "g(name,1,dataTop)t(text)g(name,1,dataTop)t(text)g(name,2,dataBottom)t(text)"
-    if deviceManager.decodeData() == (['dataTop', 'dataTop'], ['dataBottom'], 'text text text '):
-        print("TEST 1: PASS")
-    else:
-        print("TEST 1: FAIL")
-        
-    
+
+    deviceManager.terminateDevice()
+
+    deviceManager.rawData = "g(name,1,1)t(text)g(name,1,2)t(text)g(name,2,3)t(text)"
+
+    # checks the terminal output
+    expected = ' text text text'
+    output = deviceManager.decodeTerminalData()
+
+    result = "TEST 1: PASS" if output == expected else f"TEST 1: FAIL - Got ({output}) when expecting ({expected})!"
+    print(result)
+
+    # checks grpah output
+    expected = (["1", "2"], ["3"])
+    output = deviceManager.decodeGraphData()
+
+    result = "TEST 2: PASS" if output == expected else f"TEST 2: FAIL - Got ({output}) when expecting ({expected})!"
+    print(result)
