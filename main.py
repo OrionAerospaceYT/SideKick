f613c652f4b72f6168b0807a3aa1662de0ffacaa
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-10-27 12:24:08 +0100

    device + file manager

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import threading
 import time
 
 import pyqtgraph as pg
+from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtWidgets as qtw
 
@@ -29,6 +30,7 @@ class Graphing(qtw.QMainWindow):
         self.main_ui = graphing()
         self.main_ui.setupUi(self)
 
+        self.menu_width = 0
         self.running = True
         self.supported_boards = {}
 
@@ -48,15 +50,13 @@ class Graphing(qtw.QMainWindow):
 
         self.connect_buttons()
 
-        #self.main_ui.project_name.setPlaceholderText("Enter projct name here.")
+        self.main_ui.project_name.setPlaceholderText("Enter projct name here.")
         self.main_ui.message.setPlaceholderText("Enter message here.")
 
-        # self.main_ui.bottom_widget.setMinimumWidth(600)
-
-        #timer = qtc.QTimer(self)
-        # timer.setInterval(15)
-        # timer.timeout.connect(self.update)
-        # timer.start()
+        timer = qtc.QTimer(self)
+        timer.setInterval(15)
+        timer.timeout.connect(self.update)
+        timer.start()
 
     def map_top_graph(self):
         """
@@ -123,6 +123,13 @@ class Graphing(qtw.QMainWindow):
         """
 
         self.main_ui.record.clicked.connect(event_handler.record_data)
+        self.main_ui.file.clicked.connect(event_handler.open_file_manager)
+        self.main_ui.device.clicked.connect(event_handler.open_device_manager)
+        self.main_ui.new_project.clicked.connect(event_handler.new_project)
+        self.main_ui.select_project.activated[str].connect(
+            event_handler.open_file_manager)
+        self.main_ui.disconnect.clicked.connect(
+            event_handler.disconnect_device)
 
     def turn_on_rec_light(self, is_on):
         """
@@ -137,15 +144,41 @@ class Graphing(qtw.QMainWindow):
         else:
             self.main_ui.record.setStyleSheet("")
 
+    def update_projects(self):
+        """
+        Updates the current projects window so it shows all projects in the project folder.
+        """
+
+        projects_on_gui = [self.main_ui.select_project.itemText(
+            i) for i in range(self.main_ui.select_project.count())]
+        for project in event_handler.current_projects:
+            if project not in projects_on_gui:
+                self.main_ui.select_project.addItem(project)
+        for project in projects_on_gui:
+            if project not in event_handler.current_projects:
+                target = self.main_ui.select_project.findText(project)
+                self.main_ui.select_project.removeItem(target)
+
     def update(self):
         """
         calls all update functions
         """
 
-        self.update_ports()
+        # self.update_ports()
         self.update_projects()
 
-        self.main_ui.terminal.setHtml(message_handler.terminal_string)
+        # self.main_ui.terminal.setHtml(message_handler.terminal_string)
+        if self.menu_width == 0:
+            self.main_ui.file_layout.setVisible(False)
+            self.main_ui.device_layout.setVisible(False)
+        elif event_handler.device_manager:
+            self.main_ui.device_layout.setVisible(True)
+            self.main_ui.device_layout.setMinimumWidth(self.menu_width)
+            self.main_ui.file_layout.setVisible(False)
+        elif event_handler.file_manager:
+            self.main_ui.file_layout.setVisible(True)
+            self.main_ui.file_layout.setMinimumWidth(self.menu_width)
+            self.main_ui.device_layout.setVisible(False)
 
 
 class EventHandler():
@@ -157,6 +190,8 @@ class EventHandler():
     def __init__(self):
         self.recording = False
         self.light_on = False
+        self.file_manager = False
+        self.device_manager = False
 
         self.avaliable_port_list = []
         self.current_projects = []
@@ -193,14 +228,13 @@ class EventHandler():
         message_handler
         """
 
-        baud = graphing.main_ui.baud_rate.itemText(0)
+        #baud = graphing.main_ui.baud_rate.itemText(0)
 
-        print(port, baud)
+        # if port == "Select COM":
+        #    return
 
-        if port == "Select COM":
-            return
-
-        device_manager.connect_device(port, baud)
+        #device_manager.connect_device(port, baud)
+        pass
 
     def disconnect_device(self):
         """
@@ -214,8 +248,29 @@ class EventHandler():
         blinks the record light and saves the data
         """
 
-        # TODO
         self.recording = not self.recording
+
+    def open_file_manager(self):
+        """
+        opens/closes the file menu
+        also closes device manager if they are both open at the same time
+        """
+
+        self.file_manager = not self.file_manager
+
+        if self.device_manager and self.file_manager:
+            self.device_manager = False
+
+    def open_device_manager(self):
+        """
+        opens/closes the device menu
+        also closes file manager if they are both open at the same time
+        """
+
+        self.device_manager = not self.device_manager
+
+        if self.device_manager and self.file_manager:
+            self.file_manager = False
 
     def blinking_record(self):
         """
@@ -234,7 +289,7 @@ class EventHandler():
                 graphing.turn_on_rec_light(self.light_on)
                 self.light_on = not self.light_on
 
-            time.sleep(2)
+            time.sleep(1)
 
     def threaded_backend(self):
         """
@@ -252,7 +307,11 @@ class EventHandler():
 
             message_handler.organise_terminal_data()
 
-            time.sleep(1)
+            if not self.file_manager and not self.device_manager:
+                graphing.menu_width = 0
+            else:
+                graphing.menu_width = int(
+                    (graphing.main_ui.top_bar.width() / 3)) - 50
 
 
 RUNNING = True
