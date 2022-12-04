b32a33ccf3701b25836f7f0d16b2bd0174e06ee9
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-12-04 14:58:07 +0000

    Stable graphs

--- widgets.py
+++ widgets.py
@@ -16,24 +16,57 @@ COLOUR_ORDER = ["#FF0C0C",
 
 class Graph():
     """
-    Sets up the graphing object to show it on
-    the screen
+    Sets up the graphing object to show it on the screen.
+
+    Attributes:
+        in_use (bool): if the data is being read
+        key (string): the string that says the data belongs to the graph
+        graph_data (list): all of the graph data for this graph
+        plots (list): the plots on the pyqtgraph object
+        labels (list): the label for each variable on the graph
+        graph (pyqtgraph): the graph object
+        legend (pyqtgraph.addLegend): the style of the graph
+
+    Methods:
+        decode_graph_data:
+            Picks out the graph data from the raw data
+
+            Args:
+                raw_input (string): raw data in the form of "g(name,1,data)t(text)g(name,2,data)"
+            Returns:
+                graph_data (list): holds the values for the graph
+
+        set_graph_data:
+            Gets the graph data and puts it in the form to be plotted
+
+            Args:
+                raw_data (list): a list of all raw data
+
+        update_plots:
+            Adds or removes old plots.
+
+            Args:
+                num_of_plots (int): the number of plots
+
+        update_graph:
+            To update the graphs displayed on the main GUI
     """
 
     def __init__(self, key="0"):
-
-        self.legend = None
+        # variable definitions
+        self.in_use = False
+        self.key = key
+        self.graph_data = []
         self.plots = []
         self.labels = []
-        self.graph = None
 
+        # pyqtgraph definitions
         self.graph = pg.PlotWidget()
-        self.graph.setMenuEnabled(False)
-
         self.legend = self.graph.addLegend()
 
+        # Modify look of graphs
+        self.graph.setMenuEnabled(False)
         self.graph.setBackground('#2b2b35')
-
         self.legend.setLabelTextColor("#FFFFFF")
         self.graph.getAxis(
             'left').setPen(pg.mkPen(color='#FFFFFF'))
@@ -42,9 +75,6 @@ class Graph():
         self.graph.getAxis("left").setTextPen((255, 255, 255))
         self.graph.getAxis("bottom").setTextPen((255, 255, 255))
 
-        self.key = key
-        self.graph_data = []
-
     def decode_graph_data(self, raw_input):
         """
         Picks out the graph data from the raw data
@@ -52,19 +82,21 @@ class Graph():
         Args:
             raw_input (string): raw data in the form of "g(name,1,data)t(text)g(name,2,data)"
         Returns:
-            graph_top_data (list): holds the values for the top graph
-            graph_bottom_data (list): holds the values for the bottom graph
+            graph_data (list): holds the values for the graph
         """
 
         graph_data = []
         raw_list = raw_input.split("g(")
 
+        # for each item of data
         for data in raw_list:
             data = data.split(")")
             if "t(" not in data[0] and data[0] != "" and "\r" not in data[0]:
                 valid_graph_data = data[0].replace(" ", "").split(",")
+                # if the data belongs to this graph
                 if valid_graph_data[1] == self.key:
                     graph_data.append(valid_graph_data[2])
+                    # if the label is not already existing
                     if valid_graph_data[0] not in self.labels:
                         self.labels.append(valid_graph_data[0])
 
@@ -77,12 +109,15 @@ class Graph():
         Args:
             raw_data (list): a list of all raw data
         """
-        self.graph_data = []
 
-        for data in raw_data:
-            plot = self.decode_graph_data(data)
-            if plot:
-                self.graph_data.append(plot)
+        # if the graph data is not being read, change it
+        if not self.in_use:
+            self.graph_data = []
+
+            for data in raw_data:
+                plot = self.decode_graph_data(data)
+                if plot:
+                    self.graph_data.append(plot)
 
     def update_plots(self, num_of_plots):
         """
@@ -92,9 +127,11 @@ class Graph():
             num_of_plots (int): the number of plots
         """
 
+        # remove all graphs
         for item in self.plots:
             self.graph.removeItem(item)
 
+        # add new graphs
         for i in range(num_of_plots):
             self.plots.append(self.graph.plot([0],[0], name=self.labels[i]))
 
@@ -103,15 +140,30 @@ class Graph():
         To update the graphs displayed on the main GUI
         """
 
+        self.in_use = True
+
+        # transpose the 2D list of data
         plots = list(map(list, zip(*self.graph_data)))
 
+        # if there is a change in the number of plots, update
         if len(plots) != len(self.plots):
             self.update_plots(len(plots))
 
+        # replace the data with the latest data
         for index, plot in enumerate(self.plots):
+            # if the amount of plots is not equal throughout
+            # then delete all plots and break from the update loop
+            if len(plots)-1 < index:
+                self.update_plots(0)
+                self.plots = []
+                self.labels = []
+                self.graph_data = []
+                break
+            # update plot
             pen = pg.mkPen(color=COLOUR_ORDER[index%len(COLOUR_ORDER)])
             plot.setData(np.array(plots[index], dtype=float), pen=pen)
 
+        self.in_use = False
 
 class Widgets():
     """
