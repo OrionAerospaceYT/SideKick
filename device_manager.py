91689dee864adc7a31201f051168e3a7a131477f
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-01-01 01:04:56 +0000

    New connected attribute

--- device_manager.py
+++ device_manager.py
@@ -29,6 +29,22 @@ class DeviceManager():
 
         self.failed_recv = 0
 
+        self.connected = False
+
+        self.raw_cummulative_data = ""
+        self.terminal_data = ""
+
+        self.raw_data = []
+
+    def __call__(self):
+        self.device = None
+        self.get_data = None
+        self.port = None
+
+        self.failed_recv = 0
+
+        self.connected = False
+
         self.raw_cummulative_data = ""
         self.terminal_data = ""
 
@@ -45,6 +61,23 @@ class DeviceManager():
 
         self.device.write(message.encode("UTF-8"))
 
+    def debug_device(self, raw_data):
+        """
+        Prints out the raw data and the data on terminal
+
+        Args:
+            raw_data (b str): the raw data through the serial port
+        """
+        self.raw_cummulative_data += raw_data.replace(b"\r\n", b"/n").decode("UTF-8")
+
+        print("Raw data cummulative >>> " + self.raw_cummulative_data)
+
+        string = ""
+        for item in self.raw_data:
+            string += item.replace("\r\n", "")
+
+        print("Data on terminal >>> " + string)
+
     def threaded_get_raw_data(self, port, baud):
         """
         Raw input data from the serial device
@@ -55,31 +88,25 @@ class DeviceManager():
             port (string): the port to connect to
             baud (int): the baud rate of the connected device
         """
-
-        try:
-            serial.close()
-        except AttributeError:
-            pass
-
         self.port = port
-        self.terminate_device()
         self.raw_cummulative_data = ""
 
         try:
             self.device = serial.Serial(port, baud, rtscts=True)
+            self.connected = True
         except serial.SerialException as error:
             print(error)
-            self.device = None
 
         buffer = b""
 
-        while self.device is not None:
+        while self.connected:
             print("Device is threaded...")
             try:
                 raw_data = self.device.read_all()
                 self.failed_recv = 0
             except serial.SerialException as error:
                 print(error)
+                raw_data = ""
                 self.failed_recv += 1
                 if self.failed_recv == 10:
                     self.terminate_device()
@@ -91,16 +118,6 @@ class DeviceManager():
                 if buffer.startswith(b"\r\n"):
                     buffer = buffer[2:]
 
-                ############################################################################
-                # DEBUGGING CODE                                                           #
-                ############################################################################
-                #self.raw_cummulative_data += raw_data.replace(b"\r\n", b"/n").decode("UTF-8")
-                #print("Raw data cummulative >>> " + self.raw_cummulative_data)
-                #string = ""
-                #for item in self.raw_data:
-                #    string += item.replace("\r\n", "")
-                #print("Data on terminal >>> " + string)
-                ############################################################################
                 while buffer.count(b"\r\n") > 1 or buffer.endswith(b"\r\n"):
 
                     decoded_buffer = buffer.decode("UTF-8")
@@ -119,7 +136,11 @@ class DeviceManager():
 
                     if len(self.raw_data) > 1500:
                         self.raw_data.pop(0)
-        print("Ended...")
+
+        if self.device is not None:
+            self.device.close()
+
+        self()
 
     def terminate_device(self):
         """
@@ -128,10 +149,7 @@ class DeviceManager():
         that there is no data
         """
 
-        self.device = None
-        self.raw_data = []
-
-        self.terminal_data = ""
+        self.connected = False
 
     def connect_device(self, port="COM1", baud=115200):
         """
