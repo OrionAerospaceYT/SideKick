8a8a3d9934c16e05283c6f7f15690bde40006c1a
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-01-19 09:07:45 +0000

    Update file_manager.py
    Fixed FileNotFoundError

--- file_manager.py
+++ file_manager.py
@@ -30,19 +30,23 @@ class SaveManager():
     def save_data(self, raw_data):
         """
         saves the raw data to the latest save_file
-        TODO
+
         Args:
             raw_data (str): the raw data from com device
         """
         self.record_status = True
+
         save_name = f"Save{len(os.listdir(self.save_folder_path))}.txt"
         save_path = f"{self.save_folder_path}{self.sep}{save_name}"
 
         if self.record_status != self.prev_record_status:
             self.create_new_file()
 
-        with open(save_path, "r", encoding="UTF-8") as save:
-            already_saved_data = save.read().splitlines()
+        try:
+            with open(save_path, "r", encoding="UTF-8") as save:
+                already_saved_data = save.read().splitlines()
+        except FileNotFoundError:
+            return
 
         unsaved_data = [item for item in raw_data if item not in already_saved_data]
 
