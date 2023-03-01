597f5526b800aba931166f1d1f17a52b0c63377e
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-01 00:25:49 +0000

    Create sensors.h

---
+++ Internals/Actuators_Test/sensors.h
@@ -0,0 +1,14 @@
+#pragma once
+//-----Internals------
+#include "utility.h"
+#include "sensor_struct.h"
+//-----Internals------
+
+// Include your sensor libraries here
+
+namespace sensors {
+void init() {}
+
+void update() {}
+
+}  // namespace sensors
