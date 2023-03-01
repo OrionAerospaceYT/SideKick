333c0755f6a3ad42e1b802ee5e2d1dd82fd6b508
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-03-01 00:25:32 +0000

    Create sub_task.h

---
+++ Internals/Actuators_Test/sub_task.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "utility.h"
+
+// This is where we place long functions or repeated code
+/*Say hypothetically you have a filtering process you apply to your data 10
+times for a loop (its a hypothetical) you might do that in here to keep the
+task.h file cleaner
+*/
+namespace subtask {
+void exampleLongFunc() {}
+
+}  // namespace subtask
