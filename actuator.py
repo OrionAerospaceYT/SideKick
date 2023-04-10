6705836d470c3aa28f26c046ef56e0d66d8099c4
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-04-10 21:55:12 +0100

    Update actuator.py

--- actuator.py
+++ actuator.py
@@ -9,9 +9,39 @@ from PyQt5 import QtWidgets as qtw
 
 from Ui.NewActuatorUi import Ui_MainWindow as actuator
 
+class Slider:
+    """
+    Creates sliders for the actuator test window.
+    """
+    def __init__(self, name, minimum, maximum, parent=None):
+        mid_point = (minimum + maximum) // 2
+
+        self.horizontal_layout = qtw.QHBoxLayout()
+
+        self.name_label = qtw.QLabel(name)
+
+        self.slider = qtw.QSlider(parent)
+        self.slider.setOrientation(qtc.Qt.Horizontal)
+        self.slider.setMinimum(minimum)
+        self.slider.setMaximum(maximum)
+        self.slider.setValue(mid_point)
+
+        self.pos_label = qtw.QLabel().setText(str(mid_point))
+
+        spacer = qtw.QSpacerItem(20,
+                                40,
+                                qtw.QSizePolicy.Minimum,
+                                qtw.QSizePolicy.Maximum)
+
+        self.horizontal_layout.addWidget(self.name_label)
+        self.horizontal_layout.addWidget(self.slider)
+        self.horizontal_layout.addWidget(self.pos_label)
+        self.horizontal_layout.addItem(spacer)
+
+
 class ActuatorGUI(qtw.QMainWindow):
     """
-    a class
+    A class to show and control the test actuator window
     """
 
     def __init__(self, device_manager, parent=None):
@@ -33,11 +63,6 @@ class ActuatorGUI(qtw.QMainWindow):
         self.min = 0
         self.max = 180
 
-        self.actuators_ui.name.setPlaceholderText("Name")
-        self.actuators_ui.pin.setPlaceholderText("Pin")
-        self.actuators_ui.min.setPlaceholderText("Minimum")
-        self.actuators_ui.max.setPlaceholderText("Maximum")
-
         self.actuators_ui.progressBar.setMinimum(0)
         self.actuators_ui.progressBar.setMaximum(0)
 
@@ -53,6 +78,16 @@ class ActuatorGUI(qtw.QMainWindow):
         self.timer.timeout.connect(self.display_all)
         self.timer.start()
 
+    def set_place_holder_text(self):
+        """
+        Sets all of the place holder text for the line edits on
+        the window
+        """
+        self.actuators_ui.name.setPlaceholderText("Name")
+        self.actuators_ui.pin.setPlaceholderText("Pin")
+        self.actuators_ui.min.setPlaceholderText("Minimum")
+        self.actuators_ui.max.setPlaceholderText("Maximum")
+
     def set_done_upload(self):
         """
         Sends the command to stop loading and show scroll bars
@@ -63,11 +98,13 @@ class ActuatorGUI(qtw.QMainWindow):
         """
         Stops loading and shows scroll bars
         """
-        if self.done:
-            self.actuators_ui.loading.setVisible(False)
-            self.actuators_ui.scrollArea.setVisible(True)
-            self.actuators_ui.options_widget.setVisible(True)
-            self.timer.stop()
+        if not self.done:
+            return
+
+        self.actuators_ui.loading.setVisible(False)
+        self.actuators_ui.scrollArea.setVisible(True)
+        self.actuators_ui.options_widget.setVisible(True)
+        self.timer.stop()
 
     def set_progress(self, value):
         """
@@ -83,36 +120,17 @@ class ActuatorGUI(qtw.QMainWindow):
         Sends the message to the device.
         """
         self.device_manager.send(f"servo{indx}-{value}")
+        self.sliders[indx].pos_label.setText(str(value))
 
     def create_new_slider(self, name, minimum, maximum):
         """
         Creates the layout for a new slider.
         """
-        horizontal_layout = qtw.QHBoxLayout()
 
-        name_label = qtw.QLabel(name)
-
-        slider = qtw.QSlider(self.actuators_ui.scrollAreaWidgetContents)
-        slider.setOrientation(qtc.Qt.Horizontal)
-        slider.setMinimum(minimum)
-        slider.setMaximum(maximum)
-        slider.setValue((minimum + maximum) // 2)
-        slider.valueChanged.connect(lambda: self.update_pos(slider.value(),
-                                                            self.sliders.index(horizontal_layout)))
-
-        pos_label = qtw.QLabel()
-
-        spacer = qtw.QSpacerItem(20,
-                                40,
-                                qtw.QSizePolicy.Minimum,
-                                qtw.QSizePolicy.Maximum)
-
-        horizontal_layout.addWidget(name_label)
-        horizontal_layout.addWidget(slider)
-        horizontal_layout.addWidget(pos_label)
-        horizontal_layout.addItem(spacer)
-
-        return horizontal_layout
+        slider = Slider(name, minimum, maximum, self.actuators_ui.scrollAreaWidgetContents)
+        slider.slider.valueChanged.connect(lambda: self.update_pos(slider.slider.value(),
+                                            self.sliders.index(slider.horizontal_layout)))
+        return slider
 
     def add_new_actuator(self):
         """
@@ -131,7 +149,8 @@ class ActuatorGUI(qtw.QMainWindow):
         self.actuators[name] = [pin, minimum, maximum]
         self.device_manager.send(f"addservo-{pin}")
 
-        self.sliders.append(self.create_new_slider(name, minimum, maximum))
+        slider = self.create_new_slider(name, minimum, maximum)
+        self.sliders.append(slider.horizontal_layout)
         self.actuators_ui.verticalLayout_2.addLayout(self.sliders[-1])
 
         self.actuators_ui.name.setText("")
