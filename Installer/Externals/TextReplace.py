ec4f970bf66552835ee93435b510adb3397ee720
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-07-25 22:29:52 +0100

    Delete

--- Installer/Externals/TextReplace.py
+++
@@ -1,9 +0,0 @@
-import os
-
-user_name = os.getlogin()
-
-with open("arduino-cli.yaml", "r") as arduino:
-    new_arduino = arduino.read().replace("REPLACE_USER", f"{user_name}")
-
-with open("arduino-cli.yaml", "w") as arduino:
-    arduino.write(new_arduino)
