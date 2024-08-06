35d92d37f5dec00e26628a146835aa5677eeefd8
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2024-08-06 21:21:03 +0100

    Added SideKickLite class
    A class that converts a button to a checkbox

--- widgets.py
+++ widgets.py
@@ -12,6 +12,7 @@ import numpy as np
 
 from PyQt5 import QtWidgets as qtw
 
+from globals import SK_LITE_OFF_QSS, SK_LITE_ON_QSS
 from globals import GRAPH_BEGINNING, GRAPH_ENDING
 from globals import NUM_OF_DATA_PTS, COLOUR_ORDER
 
@@ -460,3 +461,33 @@ class CheckBox:
             str: the string of the currently selected version
         """
         return str(self.versions.currentText())
+
+class SideKickLite:
+    """
+    Converts the sidekick lite button to a checkbox
+    """
+
+    def __init__(self, widget, startup_state):
+        self.state = startup_state
+        self.button = widget
+        self.button.clicked.connect(self.update_state)
+
+        self.update_style_sheet()
+
+    def update_state(self):
+        """
+        Changes the boolean state variable and updates the stylesheet.
+        """
+        self.state = not self.state
+        self.update_style_sheet()
+
+    def update_style_sheet(self):
+        """
+        Sets the stylesheet to the correct type for the state.
+        """
+        if self.state:
+            self.button.setStyleSheet(SK_LITE_ON_QSS)
+            self.button.setText("SideKick Lite: ON")
+        else:
+            self.button.setStyleSheet(SK_LITE_OFF_QSS)
+            self.button.setText("SideKick Lite: OFF")
