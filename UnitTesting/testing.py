e4f8f1fb053d26d8edd3299e34f5c9b23a3f3dba
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2024-08-10 14:56:30 +0100

    Update testing.py

--- UnitTesting/testing.py
+++ UnitTesting/testing.py
@@ -20,14 +20,14 @@ class SideKickGuiTester(unittest.TestCase):
     Testing the main features and backend of the SideKickGUI.
     """
 
-    def setUp(self, test_gui):
-        self.gui = test_gui
+    def setUp(self):
+        self.gui = MainGUI()
 
     def testing_tests(self):
         self.assertEqual(0,0)
 
-if __name__ == "__main__":
+    def tearDown(self):
+        self.gui.close_gui()
 
-    test_gui = MainGUI()
-    unittest.main(test_gui)
-    test_gui.close_gui()
+if __name__ == "__main__":
+    unittest.main()
